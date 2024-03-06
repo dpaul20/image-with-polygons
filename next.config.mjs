a086487eb094ece1217fea68d9929af1aaaf7e4b
Deivy Gutierrez <deivy.gutierrez@darwoft.com>
2024-03-06 19:41:46 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+import withTM from "next-transpile-modules";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = withTM(["konva", "react-konva"])({
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: "node-loader",
+    });
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ["style-loader", "css-loader", "postcss-loader"],
+    });
+
+    return config;
+  },
+});
 
 export default nextConfig;
---
+++ src/app/components/polygons/ImageWithPolygons.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React, { useState } from "react";
+import { Stage, Layer, Line, Image as KonvaImage } from "react-konva";
+
+interface Polygon {
+  box: {
+    topX: number;
+    topY: number;
+    bottomX: number;
+    bottomY: number;
+  };
+  label: string;
+  score: number;
+  polygon: number[][];
+}
+
+/**
+ * Component that displays an image with polygons.
+ */
+const ImageWithPolygons: React.FC = () => {
+  const [image, setImage] = useState<HTMLImageElement | null>(null);
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  const [boxes, setBoxes] = useState<Polygon[]>([]);
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        if (typeof reader.result === "string") {
+          const img = new window.Image();
+          img.src = reader.result;
+          img.onload = () => {
+            setDimensions({ width: img.width, height: img.height });
+            setImage(img);
+          };
+        }
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleLoadBoxes = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const file = event.target.files[0];
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        if (typeof reader.result === "string") {
+          const data = JSON.parse(reader.result);
+          setBoxes(data.boxes);
+        }
+      };
+
+      reader.readAsText(file);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Image with polygons</h1>
+      <p>Upload an image and a JSON file with boxes</p>
+      <input type="file" accept="image/*" onChange={handleImageUpload} />
+      <p>Upload a JSON file with boxes</p>
+      <input type="file" accept="application/json" onChange={handleLoadBoxes} />
+      {image && (
+        <Stage width={dimensions.width} height={dimensions.height}>
+          <Layer>
+            <KonvaImage image={image} />
+            {boxes.map((box, i) => (
+              <Line
+                key={`box-${i}`}
+                points={box.polygon.flat()}
+                fill="red"
+                closed
+                scaleX={dimensions.width}
+                scaleY={dimensions.height}
+              />
+            ))}
+          </Layer>
+        </Stage>
+      )}
+    </div>
+  );
+};
+
+export default ImageWithPolygons;
